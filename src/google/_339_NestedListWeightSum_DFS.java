57cc0d9dbb1acc4a9662ad18517c9966afcdd129
Luke Zhang <zzhixin@amazon.com>
2019-10-23 00:23:04 -0700

    recursion easy 339

---
+++ src/google/_339_NestedListWeightSum_DFS.java
@@ -0,0 +1,22 @@
+package google;
+
+/**
+ * @author Luke Zhang
+ * @date $(DATE) $(TIME)
+ */
+public class _339_NestedListWeightSum_DFS {
+//    int sum = 0;
+//    public int depthSum(List<NestedInteger> nestedList) {
+//        helper(nestedList, 1);
+//        return sum;
+//    }
+//
+//    public void helper(List<NestedInteger> nestedList, int depth){
+//        for(NestedInteger n : nestedList){
+//            if(n.isInteger()){   // [1]
+//                sum += depth * n.getInteger();
+//            }else
+//                helper(n.getList(), depth+1);
+//        }
+//    }
+}
