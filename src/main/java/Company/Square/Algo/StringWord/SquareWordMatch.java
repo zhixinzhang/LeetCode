8c7c7b4cb7cf6309107e8718e0bbaa397fba8fd9
zhixinzhang <zhangzhixinluke@gmail.com>
2023-06-07 18:19:13 -0700

    daily practic

--- src/main/java/Company/Square/Algo/StringWord/SquareWordMatch.java
+++ src/main/java/Company/Square/Algo/StringWord/SquareWordMatch.java
@@ -10,7 +10,7 @@ import java.util.Map;
  * 不需要map，每个字符串 从左到右扫一遍 计算多少个不同
  * 
 1.2 给你square这个词，再给别的一堆词swuare之类的，w 和q在键盘上比较近，close typo，返回只有一个close typo的词
-1.3 给你square，找出squares，squre，这种删减只有一个的词
+1.3 给你square，找出squares，squre，这种删减只有一个的词   two pointer scan from left to right
  * 
  * 
 */
@@ -20,7 +20,7 @@ public class SquareWordMatch {
         findCorrectWords_Delete("square", Arrays.asList("squares", "squre", "sqaur", "Squae"));
     }
 
-    //1.1
+    //1.1 找出只有一个typo的词， 注意大小写  不需要map，每个字符串 从左到右扫一遍 计算多少个不同
     private static void findCorrectWords(String original, List<String> dictionary){
         if (dictionary == null || dictionary.size() == 0 || original.length() == 0) {
             return;
@@ -55,6 +55,7 @@ public class SquareWordMatch {
 
     //1.2
     static Map<Character, int[]> charCache = new HashMap<>();
+    static String[] lines = new String[]{"qwertyuiop", "asdfghhjkl", "zxcvbnm"};
     
     private static void findCorrectWordsCloest(String original, List<String> dictionary){
         
@@ -62,14 +63,15 @@ public class SquareWordMatch {
             return;
         }
 
-        charCache.put('q', new int[]{0, 0});
-        charCache.put('w', new int[]{0, 1});
-        charCache.put('e', new int[]{0, 2});
-        charCache.put('s', new int[]{1, 1});
+        for (int i = 0; i < lines.length; i++){
+            for (int j = 0; j < lines[i].length(); j++){
+                charCache.put(lines[i].charAt(j), new int[]{i, j});
+            }
+        }
 
         for (String target : dictionary){
             if ( original.length()  == target.length()) {
-                int distance = findDistanceWithDelete(target, original);
+                int distance = findClosetDistancee(target, original);
                 if (distance == 1) {
                     System.out.println(target);
                 }
@@ -77,8 +79,8 @@ public class SquareWordMatch {
         }
     }
 
-    private static int findDistanceWithCloset(String target, String original){
-        int ans = 0;
+    private static int findClosetDistancee(String target, String original){
+        int distance = 0;
         
         for (int i = 0, j = 0; i < original.length() && j < target.length(); i++, j++){
             // ignore upper or lower letter
@@ -88,23 +90,24 @@ public class SquareWordMatch {
             int[] tarPosition = charCache.get(targetChar);
             
             if (oriChar != targetChar) {
-                ans ++;
+                distance ++;
                 if (oriPosition[0] == tarPosition[0] && Math.abs(oriPosition[1] - tarPosition[1]) != 1 || 
                 oriPosition[1] == tarPosition[1] && Math.abs(oriPosition[0] - tarPosition[0]) != 1) {
-                    ans ++;
+                    distance ++;
                     break;
                 }
             }
 
-            if (ans >= 2) {
+            if (distance >= 2) {
                 break;
             }
         }
-        return ans;
+        
+        return distance;
     }
 
     
-        //1.3
+        //1.3  给你square，找出squares，squre，这种删减只有一个的词  two pointer
     private static void findCorrectWords_Delete(String original, List<String> dictionary){
         if (dictionary == null || dictionary.size() == 0 || original.length() == 0) {
             return;
--- src/main/java/Company/Square/OOP/_SocksColor_Map.java
+++ src/main/java/Company/Square/OOP/_SocksColor_Map.java
@@ -2,58 +2,67 @@ package Company.Square.OOP;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// https://leetcode.com/discuss/interview-question/1555905/Square-or-Sock-Pairs
+// https://www.1point3acres.com/bbs/thread-963175-1-1.html
 public class _SocksColor_Map {
+    static class Sock{
+        String color;
+        String side;
+        int index;
+        public Sock(int index, String side, String color){
+            this.index = index;
+            this.color = color;
+            this.side = side;
+        }
+    }
     public static void main(String[] args) {
-        String[][] inputs = new String[][]{
-            {"1", "left", "black"},
-            {"2", "right", "pink"},
-            {"3", "left", "pink"},
-            {"4", "right", "black"},
-            {"5", "right", "black"}
-        };
+        List<Sock> socks = Arrays.asList(
+            new Sock(1, "left", "black"),
+            new Sock(2, "right", "pink"),
+            new Sock(3, "left", "pink"),
+            new Sock(4, "right", "black"),
+            new Sock(5, "right", "black"),
+            new Sock(6, "left", "orange"),
+            new Sock(7, "left", "white")
+        );
 
-        String[][] inputs2 = new String[][]{
-            {"1", "left", "white"},
-            {"2", "right", "white"},
-            {"3", "left", "pink"},
-            {"4", "right", "black"},
-            {"5", "right", "pink"},
-            {"6", "right", "yellow"},
-            {"7", "left", "orange"}
-        };
-
+        List<Sock> socks2 = Arrays.asList(
+            new Sock(1, "left", "white"),
+            new Sock(2, "right", "white"),
+            new Sock(3, "left", "pink"),
+            new Sock(4, "right", "black"),
+            new Sock(5, "right", "pink"),
+            new Sock(6, "right", "yellow"),
+            new Sock(7, "left", "orange")
+        );
         
-        findPairs(inputs);      // [4, 1]  [3, 2]
-        findPairs(inputs2);      // [2, 1]  [3, 5]
+        findPairs(socks);      // [4, 1]  [3, 2]
+        findPairs(socks);      // [2, 1]  [3, 5]
 
         String[][] rules = new String[][]{{"orange", "black"}, {"black", "red"}};
         System.out.println("follow up");
-        findPairsFollow(inputs2, rules);
+        findPairsFollow(socks2, rules);
     }
 
-    private static List<int[]> findPairs(String[][] inputs){
+    private static List<int[]> findPairs(List<Sock> socks){
         List<int[]> ans = new ArrayList<>();
         Map<String, ArrayDeque<Integer>> cache = new HashMap<>();
-        for (String[] inp : inputs){
-            int index = Integer.valueOf(inp[0]);
-            String side = inp[1];
-            String color = inp[2];
+        for (Sock sock : socks){
+            int index = sock.index;
+            String side = sock.side;
+            String color = sock.color;
             String curSock = side + " : " + color;      // left : black  1   -> right : black 
             String otherSide = side == "right" ? "left" : "right";
             String expectedSock = otherSide + " : " + color;
             
             if (cache.containsKey(expectedSock)){
-                Deque<Integer> q = cache.get(expectedSock);
-                int expectedSockIndes = q.pollFirst();
-                ans.add(new int[]{index, expectedSockIndes});
-                if (q.isEmpty()) {
-                    cache.remove(expectedSock);
-                }
+                ans = updateSockCache(cache, expectedSock, ans, index);
             } else {
                 cache.put(curSock, new ArrayDeque<>());
                 cache.get(curSock).addLast(index);
@@ -71,9 +80,7 @@ public class _SocksColor_Map {
         }
     }
 
-    // static Map<String, ArrayDeque<Integer>> cache = new HashMap<>();
-    // static Map<String, List<String>> swapCache = new HashMap<>();
-    private static List<int[]> findPairsFollow(String[][] inputs, String[][] rules){
+    private static List<int[]> findPairsFollow(List<Sock> socks, String[][] rules){
         List<int[]> ans = new ArrayList<>();
         Map<String, ArrayDeque<Integer>> cache = new HashMap<>();
         Map<String, List<String>> swapCache = new HashMap<>();
@@ -84,10 +91,10 @@ public class _SocksColor_Map {
             swapCache.get(rule[1]).add(rule[0]);
         }
 
-        for (String[] inp : inputs){
-            int index = Integer.valueOf(inp[0]);
-            String side = inp[1];
-            String color = inp[2];
+        for (Sock sock : socks){
+            int index = sock.index;
+            String side = sock.side;
+            String color = sock.color;
             String curSock = side + " : " + color;      // left : black  1   -> right : black 
             String otherSide = side == "right" ? "left" : "right";
             String expectedSock = otherSide + " : " + color;
