48c4d12dae57118f269493de5e8c1226dfca0d8d
zhixinzhang <zhangzhixinluke@gmail.com>
2023-08-07 22:49:35 -0700

    rivian

---
+++ src/main/java/Company/Rivian/_114_FlattenBinaryToLinkedList_Preorder_MorrisTraversal.java
@@ -0,0 +1,52 @@
+package Company.Rivian;
+
+import Company.Google.TreeNode;
+
+/**
+ * @Author: Luke(New Man) Zhang
+ * @Date: 2021/1/6 20:12
+ * Link :
+ * Description:
+ */
+public class _114_FlattenBinaryToLinkedList_Preorder_MorrisTraversal {
+
+    public void flatten(TreeNode root) {
+        preorderDfs(root);
+    }
+
+    private TreeNode preorderDfs(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode newLeft = preorderDfs(root.left);
+        TreeNode newRight = preorderDfs(root.right);
+
+        if (newLeft != null) {
+            // reach to the leaf node
+            TreeNode leaf = newLeft;
+            while (leaf.right != null) {
+                leaf = leaf.right;
+            }
+            leaf.right = newRight;
+            root.right = newLeft;
+            root.left = null;
+        }
+        return root;
+    }
+
+
+    public void flatten_MorrisTraversal(TreeNode root) {
+        while(root!=null){
+            if(root.left!=null){
+                TreeNode pre = root.left;
+                while(pre.right!=null)
+                    pre = pre.right;
+                pre.right = root.right;
+                root.right = root.left;
+                root.left = null;
+            }
+            root = root.right;
+        }
+    }
+}
---
+++ src/main/java/Company/Rivian/_34_FindFirstandLastPositionofElementinSortedArray_BS.java
@@ -0,0 +1,33 @@
+package Company.Rivian;
+
+/**
+ * Created by zhang on 2018/9/18.
+ */
+public class _34_FindFirstandLastPositionofElementinSortedArray_BS {
+    public static void main(String[] args){
+        searchRange(new int[]{5,7,7,8,8,10}, 8);
+    }
+    public static int[] searchRange(int[] nums, int target) {
+        if(nums == null || nums.length == 0)
+            return new int[]{-1,-1};
+        int r = bs(nums,target);
+        int l = bs(nums,target-1)+1;
+        if (l > r)
+            return new int[]{-1,-1};
+        int[] res = new int[]{l,r};
+        return res;
+    }
+
+    public static int bs(int[] nums, int target){
+        int l = 0, r = nums.length-1;
+        int mid;
+        while(l <= r){
+            mid = l + (r - l) / 2;
+            if(nums[mid] <= target)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return l - 1;
+    }
+}
